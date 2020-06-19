3d4c6fc13bb398fffb4af66e2e15796230a10e76
Vinicius Samuel <vsinvestidor@gmail.com>
2020-06-19 20:51:33 -0300

    build

---
+++ build/precache-manifest.a14253cf331e26d454326eb56a9c8ba6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb9c8f9498119789b0d329fb955b266",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07ab2ef4f96af88664e5",
+    "url": "/static/js/2.c5a0885f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c5a0885f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d392d4a2941edbeb748d",
+    "url": "/static/js/main.25b13fbf.chunk.js"
+  },
+  {
+    "revision": "5a6b71cf8bcdeadead1b",
+    "url": "/static/js/runtime-main.a74facfc.js"
+  },
+  {
+    "revision": "207ba27766f8967dbf5f15614cf423a9",
+    "url": "/static/media/Logo.207ba277.svg"
+  }
+]);
